dd2171d3babd391de383f66b632502978680ac54
Wilkor <wilkorguitarrista@msn.com>
2023-10-04 11:55:09 -0300

    melhorias
    mudando o nome para Desk Forms

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.65d731f0448697ff1a6a5a7cd4e85f98.js"
+  "./precache-manifest.df51be5ac7d26847954e79f2f67381b6.js"
 );
 
 self.addEventListener('message', (event) => {
